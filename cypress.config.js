43aa9eee00a3aaa389195cd991de957e424a7b12
elianmanubens12 <elianmanubens10@gmail.com>
2023-09-07 17:08:14 -0300

    se agrego un test funcional

--- cypress.config.js
+++ cypress.config.js
@@ -1,16 +1,18 @@
 const { defineConfig } = require("cypress");
-import createBundler from "@bahmutov/cypress-esbuild-preprocessor";
-import addCucumberPreprocessorPlugin from "@badeball/cypress-cucumber-preprocessor";
-import createEsbuildPlugin from "@badeball/cypress-cucumber-preprocessor/esbuild";
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const addCucumberPreprocessorPlugin =
+  require("@badeball/cypress-cucumber-preprocessor").addCucumberPreprocessorPlugin;
+const createEsbuildPlugin =
+  require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
   e2e: {
-    specPattern: "cypress/e2e/features/**.features",
+    specPattern: "cypress/e2e/features/**/*.feature",
     async setupNodeEvents(on, config) {
-      const bundler = createBlunder({
-        plugins: [createEsbuildPlugin(confug)],
+      const bundler = createBundler({
+        plugins: [createEsbuildPlugin(config)],
       });
-      on("file:preprocessor", blunder);
+      on("file:preprocessor", bundler);
       await addCucumberPreprocessorPlugin(on, config);
       return config;
     },
